9ba84f98577d7eb682cab20f134821c1bdeb0ed6
Shibarashii <147302321+Shibarashii@users.noreply.github.com>
2024-11-20 21:45:38 +0800

    Add files via upload

--- binomial_distribution_calculator/manage.py
+++ binomial_distribution_calculator/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'binomial_distribution_calculator.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'binomial_distribution_calculator.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
